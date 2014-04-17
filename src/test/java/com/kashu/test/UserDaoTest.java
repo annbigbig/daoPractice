27c4a0dc419a9590e16d8e29976995cdf74db30d
Annbigbig <annbigbig@gmail.com>
2014-04-17 22:22:06 +0800

    changes at 2014-04-17 22:21:34

--- src/test/java/com/kashu/test/UserDaoTest.java
+++ src/test/java/com/kashu/test/UserDaoTest.java
@@ -1,5 +1,6 @@
 package com.kashu.test;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -16,6 +17,14 @@ public class UserDaoTest {
 		ctx.refresh();
 
 		Dao<User> userDao = ctx.getBean("userDao",Dao.class);
+		
+		// find all users
+		listUsers(userDao.findAll());
+		
+		// add a new user
+		userDao.insert(getUser1());
+		
+		// find all users
 		listUsers(userDao.findAll());
 	}
 
@@ -24,4 +33,21 @@ public class UserDaoTest {
 			System.out.println(user);
 		}
 	}
+	
+	public static User getUser1(){
+		User user = new User();
+		user.setUuid("WaluSadi");
+		user.setPasswd("walupasswd");
+		user.setFirstName("Wa");
+		user.setLastName("LuSadi");
+		user.setDisplayName("WLSD");
+		user.setMale(true);
+		user.setBirthday(new Date());
+		user.setAddress("我家就住在那太麻里隔壁");
+		user.setPhone("12345678");
+		user.setMobile("87654321");
+		user.setScore(1200);
+		return user;
+	}
+	
 }
