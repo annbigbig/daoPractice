517efa09c586f8faad1cb1086997384700e2b087
Annbigbig <annbigbig@gmail.com>
2014-04-20 21:38:38 +0800

    changes at 2014-04-20 21:38:32

--- src/main/java/com/kashu/dao/jdbc/annotation/UserDaoImpl.java
+++ src/main/java/com/kashu/dao/jdbc/annotation/UserDaoImpl.java
@@ -2,6 +2,7 @@ package com.kashu.dao.jdbc.annotation;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,7 +13,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -24,6 +27,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Propagation;
 
 import com.kashu.dao.Dao;
+import com.kashu.dao.jdbc.annotation.domain.Article;
+import com.kashu.dao.jdbc.annotation.domain.Category;
+import com.kashu.dao.jdbc.annotation.domain.Group;
 import com.kashu.dao.jdbc.annotation.domain.User;
 
 @Repository("userDao")
@@ -78,7 +84,8 @@ public class UserDaoImpl implements Dao<User> ,InitializingBean {
 	}
 
 	public List<User> queryAllWith() {
-		String sql = "";
+		Map<String,Object> params = new HashMap<String,Object>();
+		String sql = getJoinSQL(params);
 		return null;
 	}
 
@@ -285,20 +292,73 @@ public class UserDaoImpl implements Dao<User> ,InitializingBean {
 		return sql;
 	}
 	
+	//  [TB_USER] --- [JN_USER_GROUP] --- [TB_GROUP] --- [JN_USER_USER] --- [TB_USER] --- [TB_ARTICLE] 
 	public String getJoinSQL(Map<String,Object> params){
 		String sql = "SELECT ";
 			sql += "u.ID, u.UUID, u.PASSWD, u.FIRST_NAME, u.LAST_NAME, u.DISPLAY_NAME, ";
 			sql += "u.MALE, u.BIRTHDAY, u.ADDRESS, u.PHONE, u.MOBILE, u.SCORE, ";
-			sql += "g.ID as GROUP_ID, g.NAME as GROUP_NAME,";
-			sql += "u2.ID as FRIEND_ID, u2.UUID as FRIEND_UUID, u2.FIRST_NAME as FRIEND_FIRST_NAME,";
+			sql += "g.ID as GROUP_ID, g.NAME as GROUP_NAME, ";
+			sql += "u2.ID as FRIEND_ID, u2.UUID as FRIEND_UUID, u2.FIRST_NAME as FRIEND_FIRST_NAME, ";
 			sql += "u2.LAST_NAME as FRIEND_LAST_NAME, u2.DISPLAY_NAME as FRIEND_DISPLAY_NAME, ";
-		
-		return null;
+			sql += "u2.MALE as FRIEND_MALE, u2.BIRTHDAY as FRIEND_BIRTHDAY, u2.ADDRESS as FRIEND_ADDRESS, ";
+			sql += "u2.PHONE AS FRIEND_PHONE, u2.MOBILE as FRIEND_MOBILE, u2.SCORE as FRIEND_SCORE, ";
+			sql += "a.ID as ARTICLE_ID, a.SUBJECT as ARTICLE_SUBJECT, a.CONTENT as ARTICLE_CONTENT, ";
+			sql += "a.CREATED_TIME as ARTICLE_CREATED_TIME, a.LAST_MODIFIED as ARTICLE_LAST_MODIFIED, ";
+			sql += "a.CATEGORY_ID as ARTICLE_CATEGORY_ID ";
+			sql += "c.ID as CATEGORY_ID, c.NAME as CATEGORY_NAME, c.PID as CATEGORY_PID";
+			sql += "FROM TB_USER u ";
+			sql += "LEFT OUTER JOIN JN_USER_GROUP ug ON (u.ID = ug.USER_ID) ";
+			sql += "LEFT OUTER JOIN TB_GROUP g ON (ug.GROUP_ID = g.ID) ";
+			sql += "LEFT OUTER JOIN JN_USER_USER uu ON (uu.UID_1 = u.ID) ";
+			sql += "LEFT OUTER JOIN TB_USER u2 ON (uu.UID_2 = u2.ID) ";
+			sql += "LEFT OUTER JOIN TB_ARTICLE a ON (a.USER_ID = u.ID)";
+			sql += "LEFT OUTER JOIN TB_CATEGORY c ON (a.CATEGORY_ID = c.ID)";
+			
+			String operator = (params.get("operator")==null) ? "" : (String) params.get("operator");
+			String where = "";
+			if (params.get("id")!=null){
+				where = " WHERE u.ID = :id";
+			}else if (params.get("uuid")!=null){
+				where = " WHERE u.UUID LIKE :uuid";
+			}else if (params.get("firstName")!=null){
+				where = " WHERE u.FIRST_NAME LIKE :firstName";
+			}else if(params.get("lastName")!=null){
+				where = " WHERE u.LAST_NAME LIKE :lastName";
+			}else if(params.get("displayName")!=null){
+				where = " WHERE u.DISPLAY_NAME LIKE :displayName";
+			}else if(params.get("male")!=null){
+				where = " WHERE u.MALE = :male";
+			}else if(params.get("birthday")!=null && operator.equals("gt")){
+				where = " WHERE u.BIRTHDAY > :birthday";
+			}else if(params.get("birthday")!=null && operator.equals("lt")){
+				where = " WHERE u.BIRTHDAY < :birthday";
+			}else if(params.get("birthday")!=null && operator.equals("eq")){
+				where = " WHERE u.BIRTHDAY = :birthday";
+			}else if(params.get("date1")!=null && params.get("date2")!=null){
+				where = " WHERE u.BIRTHDAY BETWEEN :date1 AND :date2";
+			}else if(params.get("address")!=null){
+				where = " WHERE u.ADDRESS LIKE :address";
+			}else if(params.get("phone")!=null){
+				where = " WHERE u.PHONE LIKE :phone";
+			}else if(params.get("mobile")!=null){
+				where = " WHERE u.MOBILE LIKE :mobile";
+			}else if(params.get("score")!=null && operator.equals("gt")){
+				where = " WHERE u.SCORE > :score";
+			}else if(params.get("score")!=null && operator.equals("lt")){
+				where = " WHERE u.SCORE < :score";
+			}else if(params.get("score")!=null && operator.equals("eq")){
+				where = " WHERE u.SCORE = :score";
+			}
+			
+			sql += where;
+			sql += " ORDER BY u.ID, g.ID, u2.ID, a.ID ASC;";
+			System.out.println("[Join SQL] = " + sql);
+
+		return sql;
 	}
 	
-	//Inner Class，將資料庫取出的每一行Row封裝成對應的Usert物件
+	//Inner Class，將資料庫取出的每一行Row封裝成對應的Usert物件 (僅適用於查詢 TB_USER單表時)
 	private static final class UserMapper implements RowMapper<User> {
-
 		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 			User user = new User();
 			user.setId(rs.getLong("ID"));
@@ -315,7 +375,76 @@ public class UserDaoImpl implements Dao<User> ,InitializingBean {
 			user.setScore(rs.getInt("SCORE"));
 			return user;
 		}
-		
 	}
 
+	private static final class UserExtractor implements ResultSetExtractor<List<User>> {
+		public List<User> extractData(ResultSet rs) throws SQLException,DataAccessException {
+			Map<Long,User> userMap = new HashMap<Long,User>();
+			User user = null;
+			Group group = null;
+			User friend = null;
+			Article article = null;
+			Category category = null;
+			
+			while(rs.next()){
+				Long id = rs.getLong("ID");
+				if(userMap.get(id)==null){	//new user
+					user = new User();
+					user.setId(rs.getLong("ID"));
+					user.setUuid(rs.getString("UUID"));
+					user.setPasswd(rs.getString("PASSWD"));
+					user.setFirstName(rs.getString("FIRST_NAME"));
+					user.setLastName(rs.getString("LAST_NAME"));
+					user.setDisplayName(rs.getString("DISPLAY_NAME"));
+					user.setMale(rs.getBoolean("MALE"));
+					user.setBirthday(rs.getDate("BIRTHDAY"));
+					user.setAddress(rs.getString("ADDRESS"));
+					user.setPhone(rs.getString("PHONE"));
+					user.setMobile(rs.getString("MOBILE"));
+					user.setScore(rs.getInt("SCORE"));
+					userMap.put(id, user);
+				}
+				group = new Group();
+				group.setId(rs.getLong("GROUP_ID"));
+				group.setName(rs.getString("GROUP_NAME"));
+				if(!user.getGroups().contains(group) && group.getId()!=null){
+					user.getGroups().add(group);
+				}
+				friend = new User();
+				friend.setId(rs.getLong("FRIEND_ID"));
+				friend.setUuid(rs.getString("FRIEND_UUID"));
+				friend.setFirstName(rs.getString("FRIEND_FIRST_NAME"));
+				friend.setLastName(rs.getString("FRIEND_LAST_NAME"));
+				friend.setDisplayName(rs.getString("FRIEND_DISPLAY_NAME"));
+				friend.setMale(rs.getBoolean("FRIEND_MALE"));
+				friend.setBirthday(rs.getDate("FRIEND_BIRTHDAY"));
+				friend.setAddress(rs.getString("FRIEND_ADDRESS"));
+				friend.setPhone(rs.getString("FRIEND_PHONE"));
+				friend.setMobile(rs.getString("FRIEND_MOBILE"));
+				friend.setScore(rs.getInt("FRIEND_SCORE"));
+				if(!user.getFriends().contains(friend) && friend.getId()!=null){
+					user.getFriends().add(friend);
+				}
+				category = new Category();
+				category.setId(rs.getLong("CATEGORY_ID"));
+				category.setName(rs.getString("CATEGORY_NAME"));
+				
+				article = new Article();
+				article.setId(rs.getLong("ARTICLE_ID"));
+				article.setSubject(rs.getString("ARTICLE_SUBJECT"));
+				article.setContent(rs.getString("ARTICLE_CONTENT"));
+				article.setCreatedTime(rs.getDate("ARTICLE_CREATED_TIME"));
+				article.setLastModified(rs.getDate("ARTICLE_LAST_MODIFIED"));
+				article.setUser(user);
+				article.setCategory(category);
+				if(!user.getArticles().contains(article) && article.getId()!=null){
+					user.getArticles().add(article);
+				}
+			}
+			
+			return new ArrayList<User>(userMap.values());
+		}
+		
+	}
+	
 }
